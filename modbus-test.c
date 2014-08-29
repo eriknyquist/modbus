11b49ca6f154721b1036cf7e32daff5388b4b38e
erik nyquist <eriknyquist@erik-nyquists-MacBook-Pro-2.local>
2014-08-29 22:48:00 +0100

    Used test/random-test-client.c from the libmodbus source as a guide for allocating memory to store register data. Fixed the signal handler. Removed call to sleep() and added gettimeofday() from time.h for non-blocking updates.

--- modbus-test.c
+++ modbus-test.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <modbus.h>
+#include <time.h>
 #include <errno.h>
 #include <string.h>
 
@@ -11,9 +12,15 @@
 #define MB_STOPBITS 2
 #define MB_PARITY 'N'
 #define MB_SLAVE_ADDRESS 10
+#define UPDATE_FREQUENCY_HZ 10
 
-#define MB_REG_BASE 40005
-#define MB_REG_COUNT 4
+#define ADDRESS_START 0
+#define ADDRESS_END 99
+
+#define READ_BASE 5
+#define READ_COUNT 4
+
+uint8_t gotsigint = 0;
 
 void fail (modbus_t *ctx)
 {
@@ -22,13 +29,31 @@ void fail (modbus_t *ctx)
 	exit(-1);
 }
 
+double getms ()
+{
+	struct timeval time;
+	gettimeofday (&time, NULL);
+	return (time.tv_sec + (time.tv_usec / 1000000.0)) * 1000.0;
+}
+
+void siginthandler()
+{
+	gotsigint = 1;
+}
+
 int main ( int argc, char *argv[] )
 {
+	double delaytime = 1000 / UPDATE_FREQUENCY_HZ;
+	double lastupdate;
+  	int nb, n, i;
+	uint16_t *tab_registers;
 	modbus_t *ctx;
-  	uint16_t tab_reg[MB_REG_BASE + MB_REG_COUNT];
-	int n, i;
 
-	signal(SIGINT, fail(ctx));
+	nb = ADDRESS_END - ADDRESS_START;
+	tab_registers = (uint16_t *) malloc(nb * sizeof(uint16_t));
+	memset(tab_registers, 0, nb * sizeof(uint16_t));
+
+	signal(SIGINT, siginthandler);
 
 	if (argc != 2)
 	{
@@ -58,24 +83,30 @@ int main ( int argc, char *argv[] )
 
 	printf("Freq.     Current   Voltage   Motor RPM \n");
 
+	lastupdate = getms();
 	while(1)
 	{
-		n = modbus_read_registers(ctx, MB_REG_BASE, MB_REG_COUNT, tab_reg+MB_REG_BASE);
-
-		if (n <= 0)
+		if ((getms() - lastupdate) >= delaytime)
 		{
-			fprintf(stderr, "Unable to read modbus registers\n%s\n", strerror(errno));
-			fail(ctx);
+			n = modbus_read_registers(ctx, READ_BASE, READ_COUNT, tab_registers);
+
+			if (n <= 0)
+			{
+				fprintf(stderr, "Unable to read modbus registers\n%s\n", strerror(errno));
+				fail(ctx);
+			}
+
+			printf("\r");
+
+			for (i = READ_BASE; i < READ_COUNT; i++)
+			{
+				printf ( "%10d", tab_registers[i]);
+			}
+
+			fflush(stdout);
+			lastupdate = getms();
 		}
 
-		printf("\r");
-
-		for (i = MB_REG_BASE; i < MB_REG_BASE + n; i++)
-		{
-			printf ( "%10d", tab_reg[i]);
-		}
-
-		fflush(stdout);
-		sleep(1);
+		if (gotsigint) fail(ctx);
 	}
 }
