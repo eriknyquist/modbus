5f959442fb23f588519272b36f4de5b1867bf4ca
erik nyquist <eriknyquist@erik-nyquists-MacBook-Pro-2.local>
2014-08-31 01:34:31 +0100

    refactoring

--- main.c
+++ main.c
@@ -10,13 +10,42 @@
 #include "common.h"
 
 uint8_t gotsigint = 0;
+static modbus_t *mbp;
 
-float scalefactors[4] = {FREQ_RESOLUTION_HZ, CURRENT_RESOLUTION_A,
-			 VOLTAGE_RESOLUTION_V, MOTORSPEED_RESOLUTION_RPM};
+float scalefactors[4] =
+{
+	FREQ_RESOLUTION_HZ, 
+	CURRENT_RESOLUTION_A,
+	VOLTAGE_RESOLUTION_V, 
+	MOTORSPEED_RESOLUTION_RPM
+};
 
 void siginthandler()
 {
 	gotsigint = 1;
+}
+
+struct values
+{
+	uint16_t *inputs_raw;
+	float *inputs_scaled;
+};
+
+void update (uint16_t *inputs_raw, float *inputs_scaled)
+{
+	int n, i;
+	n = modbus_read_input_registers(mbp, READ_BASE, READ_COUNT, inputs_raw);
+
+	if (n <= 0)
+	{
+		fprintf(stderr, "Unable to read modbus registers\n%s\n", strerror(errno));
+		fail(mbp);
+	}
+
+	for (i = 0; i < READ_COUNT; i++)
+	{
+		inputs_scaled[i] = inputs_raw[i] * scalefactors[i];
+	}
 }
 
 int main ( int argc, char *argv[] )
@@ -30,12 +59,14 @@ int main ( int argc, char *argv[] )
 	double delaytime = 1000 / UPDATE_FREQUENCY_HZ;
 	double lastupdate;
   	int n, i;
-	uint16_t *tab_registers;
-	static modbus_t *mbp;
+	struct values *v;
 
 	/* Allocate space to store register reads */
-	tab_registers = (uint16_t *) malloc(READ_COUNT * sizeof(uint16_t));
-	memset(tab_registers, 0, READ_COUNT * sizeof(uint16_t));
+	v->inputs_raw = (uint16_t *) malloc(READ_COUNT * sizeof(uint16_t));
+	memset(v->inputs_raw, 0, READ_COUNT * sizeof(uint16_t));
+
+	v->inputs_scaled = (float *) malloc(READ_COUNT * sizeof(float));
+	memset(v->inputs_scaled, 0, READ_COUNT * sizeof(float));
 
 	/* Catch sigint (Ctrl-C) */
 	signal(SIGINT, siginthandler);
@@ -50,19 +81,11 @@ int main ( int argc, char *argv[] )
 	{
 		if ((getms() - lastupdate) >= delaytime)
 		{
-			n = modbus_read_input_registers(mbp, READ_BASE, READ_COUNT, tab_registers);
-
-			if (n <= 0)
-			{
-				fprintf(stderr, "Unable to read modbus registers\n%s\n",
-					strerror(errno));
-				fail(mbp);
-			}
-
+			update(v->inputs_raw, v->inputs_scaled);
 			printf("\r");
 			for (i = 0; i < READ_COUNT; i++)
 			{
-				printf ( "%16f", (tab_registers[i] * scalefactors[i]));
+				printf ( "%16f", (v->inputs_scaled[i]));
 			}
 
 			fflush(stdout);
