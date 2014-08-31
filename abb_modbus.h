700243777b8f0825c5e70a448940a7c736eaa741
erik nyquist <eriknyquist@erik-nyquists-MacBook-Pro-2.local>
2014-08-31 15:09:53 +0100

    refactoring, cleaning up.

--- abb_modbus.h
+++ abb_modbus.h
@@ -2,8 +2,9 @@
 #define ABB_MODBUS_H_
 
 double getms();
-void abb_modbus_init(char *serialport, modbus_t *modbusport);
+modbus_t *abb_modbus_init(char *serialport);
 void fail (modbus_t *mbp);
 void update (uint16_t *inputs_raw, float *inputs_scaled, modbus_t *mbp);
+int abb_update_input_registers (uint16_t *inputs_raw, float *inputs_scaled, modbus_t *modbusport);
 
 #endif
--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 #include "common.h"
 
 static uint8_t gotsigint = 0;
-static modbus_t *modbusport;
 
 void siginthandler()
 {
@@ -31,43 +30,28 @@ int main ( int argc, char *argv[] )
 		return -1;
 	}
 
-	double delaytime = 1000 / UPDATE_FREQUENCY_HZ;
-	double lastupdate;
-  	int n, i;
+	modbus_t *modbusport;
+  	int i, ret;
 	struct values *registers, r;
 	registers = &r;
 
 	/* Allocate space to store register reads */
-	registers->inputs_raw = (uint16_t *) malloc(READ_COUNT * sizeof(uint16_t));
-	memset(registers->inputs_raw, 0, READ_COUNT * sizeof(uint16_t));
+	registers->inputs_raw = (uint16_t *) malloc(INPUT_REG_READ_COUNT * sizeof(uint16_t));
+	memset(registers->inputs_raw, 0, INPUT_REG_READ_COUNT * sizeof(uint16_t));
 
-	registers->inputs_scaled = (float *) malloc(READ_COUNT * sizeof(float));
-	memset(registers->inputs_scaled, 0, READ_COUNT * sizeof(float));
+	registers->inputs_scaled = (float *) malloc(INPUT_REG_READ_COUNT * sizeof(float));
+	memset(registers->inputs_scaled, 0, INPUT_REG_READ_COUNT * sizeof(float));
 
 	/* Catch sigint (Ctrl-C) */
 	signal(SIGINT, siginthandler);
 
-	abb_modbus_init(argv[0], modbusport);	
+	modbusport = abb_modbus_init(argv[0]);	
 
-	printf("         Freq.(Hz)       Current(A)      Voltage(V)      Motor Speed(RPM) \n");
+	write(1, BANNER, strlen(BANNER));
 
-	/* initialise like this so we get a print immediately */
-	lastupdate = (getms() - delaytime);
 	while(1)
 	{
-		if ((getms() - lastupdate) >= delaytime)
-		{
-			update(registers->inputs_raw, registers->inputs_scaled, modbusport);
-			printf("\r");
-			for (i = 0; i < READ_COUNT; i++)
-			{
-				printf ( "%16f", (registers->inputs_scaled[i]));
-			}
-
-			fflush(stdout);
-			lastupdate = getms();
-		}
-		
+		ret = abb_update_input_registers(registers->inputs_raw, registers->inputs_scaled, modbusport);	
 		/* if we caught sigint, close modbus
 		   connections & exit gracefully */
 		if (gotsigint) fail(modbusport); 
